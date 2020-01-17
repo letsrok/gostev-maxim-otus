142fe792658ceef5366f6d0fa1a31622edd2efeb
LetsRRRok <razial74@gmail.com>
2020-01-17 13:37:58 +0500

    fixes

--- javascript/javascript-lesson-3/getPath.js
+++ javascript/javascript-lesson-3/getPath.js
@@ -2,20 +2,21 @@
 
 function getPath(item) {
 
+  if(!item) {
+    throw new Error('Элемент не найден')
+  }
+
   let path = ''
 
   function writePath(elem) {
     let itemName = elem.nodeName.toLowerCase(),
         itemParent = elem.parentNode,
-        itemNth = Number
+        itemNth = ''
 
     // Проверяем есть ли у элемента родитель
     if(itemParent && itemParent.children.length > 1) {
       itemNth = Array.prototype.indexOf.call(itemParent.children, elem)
       itemNth = `:nth-child(${itemNth+1})`
-    }
-    else {
-      itemNth = ''
     }
 
     let selector =  `${itemName}${itemNth}>`
