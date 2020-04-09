1fce2bbcf4583077c3ba32b4ba0dbef33d5959f8
LetsRRRok <razial74@gmail.com>
2020-04-09 21:36:28 +0500

    add vue-lesson-1

---
+++ vue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HomePage @btnPress="gameStart" v-if="!gameBegin"/>
+    <Game @btnPress="gameStart" v-else/>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+import Game from './components/Game.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+    Game
+  },
+  data() {
+    return {
+      gameBegin: true
+    }
+  },
+  methods: {
+    gameStart() {
+      this.gameBegin = !this.gameBegin
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/HomePage.vue
@@ -0,0 +1,79 @@
+<template>
+  <b-container class="homeform">
+    <b-row align-h="center">
+      <b-col sm='6'>
+        <h1>Привет</h1>
+        <p>Добро пожаловать на {{day}} тренировочный день</p>
+        <h3>Настройки</h3>
+        <b-form @submit.prevent="$emit('btnPress')">
+          <br>
+          <b-form-input
+             v-model="settings.testLength"
+             type="range"
+             min="1"
+             max="15"
+          ></b-form-input>
+          <p>Длительность {{settings.testLength}} минут</p>
+          <br>
+          <b-form-input
+             v-model="settings.difficulty"
+             type="range"
+             min="1"
+             max="10"
+          ></b-form-input>
+          <p>Сложность: {{settings.difficulty}}</p>
+          <br>
+          <b-form-checkbox-group
+            v-model="settings.selectedTypes"
+            :options="settings.types"
+            class="mb-3"
+            value-field="value"
+            text-field="text"
+            disabled-field="notEnabled"
+            stacked
+          ></b-form-checkbox-group>
+          <div class="mt-3">Selected: <strong>{{ settings.selectedTypes }}</strong></div>
+          <b-button
+             type="submit"
+             variant="primary form-send"
+             :disabled="settings.selectedTypes.length < 1"
+             >Начать!
+          </b-button>
+        </b-form>  
+      </b-col>
+    </b-row>
+  </b-container>
+  
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      day: 25,
+      settings: {
+        testLength: 1,
+        difficulty: 1,
+        types: [
+          {text: 'Суммирование', value: '+'},
+          {text: 'Разность', value: '-'},
+          {text: 'Умножение', value: '*'},
+          {text: 'Деление', value: '/'},
+          {text: 'Возведение в степень', value: '^'}
+        ],
+        selectedTypes: []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .homeform {
+    margin-top: 20px;
+  }
+  .form-send {
+    margin: 25px auto 0 ;
+  }
+</style>
