40af6843b8f506359292ecc527fe5ee31c05a292
LetsRRRok <razial74@gmail.com>
2020-05-17 16:46:49 +0500

    fix

---
+++ angular/angular-lesson-1/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import { ResentlyComponent } from './resently/resently.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { GameComponent } from './game/game.component';
+import { SettingsComponent } from './settings/settings.component';
+import {MatSelectModule} from '@angular/material/select';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ResentlyComponent,
+    GameComponent,
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/angular-lesson-1/src/app/resently/resently.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resently',
+  templateUrl: './resently.component.html',
+  styleUrls: ['./resently.component.scss']
+})
+export class ResentlyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
