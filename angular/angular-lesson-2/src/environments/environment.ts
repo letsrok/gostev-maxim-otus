b6d7a4d7da8c90327d41b0e086d77477c27f783d
LetsRRRok <razial74@gmail.com>
2020-05-25 18:04:19 +0500

    add angular-lesson-2

---
+++ angular/angular-lesson-2/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  API_KEY: 'trnsl.1.1.20200520T075647Z.05f9ecdf2c5076ec.f9f550024c69cb1c462fa08f447c73e09223301f',
+  API_HOST: 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
